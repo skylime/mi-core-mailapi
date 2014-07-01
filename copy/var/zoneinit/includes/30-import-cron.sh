95a2c8374d1076d31aaba4f6a2cfc9b5b9602e2a
Thomas Merkel <tm@core.io>
2014-07-01 16:56:00 +0200

    fix broken cronjob
    * if cronjob is missing it should be created and no old one should be deleted
    * because of `set -x` we use `||` to run possible fail commands also

--- copy/var/zoneinit/includes/30-import-cron.sh
+++ copy/var/zoneinit/includes/30-import-cron.sh
@@ -1,2 +1,2 @@
 CRON="0,5,10,15,20,25,30,35,40,45,50,55 * * * *        /opt/core/bin/import-cron"
-(crontab -l www 2>/dev/null; echo "$CRON" ) | sort | uniq | sudo -u www crontab
+(crontab -l www 2>/dev/null || true; echo "$CRON" ) | sort | uniq | sudo -u www crontab
