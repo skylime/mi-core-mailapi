1bb6272610a0f646c06b14b4e1068e10b609cc42
Boris Poblotzki <bp@core.io>
2014-07-25 17:01:16 +0200

    typo in django keygen zoneinit

--- copy/var/zoneinit/includes/21-generate_django_key.sh
+++ copy/var/zoneinit/includes/21-generate_django_key.sh
@@ -1,3 +1,3 @@
 sed '/^SECRET_KEY/d' /var/www/limeade3/limeade3/settings.py > /tmp/bak.settings.py \
  && mv /tmp/bak.settings.py /var/www/limeade3/limeade3/settings.py
-python -c "from django.utils.crypto import get_random_string;print 'SECRET_KEY = \"' + get_random_string(50, 'abcdefghijklmnopqrstuvwxyz0123456789\!@#$%^&*(-_=+)') + '\"'" >> /var/www/limeade3/limeade3/settings.py
+python3.3 -c "from django.utils.crypto import get_random_string;print ('SECRET_KEY = \"' + get_random_string(50, 'abcdefghijklmnopqrstuvwxyz0123456789\!@#$%^&*(-_=+)') + '\"')" >> /var/www/limeade3/limeade3/settings.py
