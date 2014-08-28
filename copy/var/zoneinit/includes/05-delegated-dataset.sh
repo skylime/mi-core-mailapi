4d78cb2fe65f28208d386ba1195217c98ddf8736
Thomas Merkel <tm@core.io>
2014-08-28 21:29:18 +0200

    add backup via znapzend

--- copy/var/zoneinit/includes/05-delegated-dataset.sh
+++ copy/var/zoneinit/includes/05-delegated-dataset.sh
@@ -5,4 +5,9 @@ DDS=zones/$UUID/data
 if zfs list $DDS 1>/dev/null 2>&1; then
 	zfs set mountpoint=/var/db/redis/ $DDS
 	chown redis /var/db/redis/
+
+	# znapzend for backup
+	znapzendzetup create --recursive --tsformat='%Y-%m-%d-%H%M%S' \
+		SRC '7day=>1hour,30day=>1day,1year=>1week,10year=>1month' ${DDS}
+	/usr/sbin/svcadm enable svc:/pkgsrc/znapzend:default
 fi
