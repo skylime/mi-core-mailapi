9ee6d3115c9d8a64672a134485b58c30d9a5dc10
Thomas Merkel <tm@core.io>
2014-08-28 21:59:32 +0200

    add --donotask to backup system

--- copy/var/zoneinit/includes/05-delegated-dataset.sh
+++ copy/var/zoneinit/includes/05-delegated-dataset.sh
@@ -7,7 +7,7 @@ if zfs list $DDS 1>/dev/null 2>&1; then
 	chown redis /var/db/redis/
 
 	# znapzend for backup
-	znapzendzetup create --recursive --tsformat='%Y-%m-%d-%H%M%S' \
+	znapzendzetup create --recursive --tsformat='%Y-%m-%d-%H%M%S' --donotask \
 		SRC '7day=>1hour,30day=>1day,1year=>1week,10year=>1month' ${DDS}
 	/usr/sbin/svcadm enable svc:/pkgsrc/znapzend:default
 fi
