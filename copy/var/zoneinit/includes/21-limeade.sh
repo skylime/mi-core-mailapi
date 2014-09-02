e347540aa60501098dcb048db549e8103855c277
Thomas Merkel <tm@core.io>
2014-09-02 16:03:04 +0200

    case required

--- copy/var/zoneinit/includes/21-limeade.sh
+++ copy/var/zoneinit/includes/21-limeade.sh
@@ -7,5 +7,5 @@ SECRET_KEY=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-100})
 /opt/local/bin/sed -i \
 	-e "s:^DEBUG.*:DEBUG = False:g" \
 	-e "s:^TEMPLATE_DEBUG.*:TEMPLATE_DEBUG = False:g" \
-	-e "s:^ALLOWED_HOSTS.*:ALLOWED_HOSTS = ['${ALLOWED_HOST}', '127.0.0.1', 'localhost']:g" \
+	-e "s:^ALLOWED_HOSTS.*:ALLOWED_HOSTS = \['${ALLOWED_HOST}', '127.0.0.1', 'localhost'\]:g" \
 	/var/www/limeade3/limeade3/settings.py
