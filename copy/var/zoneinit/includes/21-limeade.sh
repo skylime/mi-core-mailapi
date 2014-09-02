3f1846a3315a73c9e3d7187779d172671f2e26ee
Thomas Merkel <tm@core.io>
2014-09-02 14:53:53 +0200

    Create production ready config file for django

---
+++ copy/var/zoneinit/includes/21-limeade.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Make django app production ready
+
+ALLOWED_HOST=$(hostname)
+SECRET_KEY=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-100})
+
+/opt/local/bin/sed -i \
+	-e "s:^DEBUG.*:DEBUG = False:g" \
+	-e "s:^TEMPLATE_DEBUG.*:TEMPLATE_DEBUG = False:g" \
+	-e "s:^ALLOWED_HOSTS.*:ALLOWED_HOSTS = ['${ALLOWED_HOST}', '127.0.0.1', 'localhost']" \
+	/var/www/limeade3/limeade3/settings.py
