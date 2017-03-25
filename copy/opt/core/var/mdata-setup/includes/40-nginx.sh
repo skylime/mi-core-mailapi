9abd5bf4746a775b0751c7483d6c8c4acdb0d407
Thomas Merkel <tm@core.io>
2017-03-25 12:54:29 +0100

    Version bump to 16.4.0
    Provide the newest version of core-base and include Let's Encrypt support

--- copy/opt/core/var/mdata-setup/includes/40-nginx.sh
+++ copy/opt/core/var/mdata-setup/includes/40-nginx.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-# Configure nginx ssl certificate and authentication
-
-# SSL
-mdata-get nginx_ssl > /opt/local/etc/nginx/ssl/nginx.pem
-chmod 400 /opt/local/etc/nginx/ssl/nginx.pem
+# Configure nginx authentication
 
 # Basic authentication
 mdata-get nginx_auth > /opt/local/etc/nginx/auth.htpasswd
