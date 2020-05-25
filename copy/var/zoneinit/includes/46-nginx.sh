7cd0f3c536f7d70f6d26929b1c006018efa9d85b
Thomas Merkel <tm@core.io>
2020-05-25 14:22:33 +0200

    Use new SSL generation method

--- copy/var/zoneinit/includes/46-nginx.sh
+++ copy/var/zoneinit/includes/46-nginx.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+# Configure nginx authentication
+
+# Basic authentication
+if NGINX_AUTH=$(mdata-get nginx_auth 2>/dev/null); then
+	echo ${NGINX_AUTH} > /opt/local/etc/nginx/auth.htpasswd
+fi
+
+# Start nginx service
 svcadm enable svc:/pkgsrc/nginx:default
